afc02010a12d2f72efe5c8671b5e4a8a76051f57
yeeun <jovm268@naver.com>
2025-10-20 14:48:12 +0900

    feat : SVG컴포넌트화

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import Taco from '../assets/taco.svg';
-
+import Taco from '../assets/taco.svg?react';
 export default function Header() {
   return (
     <header className="flex flex-col justify-center gap-3">
-      <img src={Taco} alt="오늘뭐먹지아이콘" /> <h1>오늘 뭐먹지?</h1>
+      <Taco></Taco>
+      <h1>오늘 뭐먹지?</h1>
     </header>
   );
 }
