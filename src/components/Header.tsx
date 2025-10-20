d624af2c1606a70bb0420478472e23c0cfc9ba1a
yeeun <jovm268@naver.com>
2025-10-20 16:40:16 +0900

    feat : useEffect() axios데이터값 불러오기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Taco from '../assets/taco.svg?react';
 export default function Header() {
   return (
-    <header className="flex flex-col justify-center gap-3">
+    <header className="flex flex-col justify-center gap-3 items-center">
       <Taco></Taco>
       <h1>오늘 뭐먹지?</h1>
     </header>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,27 @@
-export default function List() {
+import type { Food } from '../App';
+
+interface ListProps {
+  allFood: Food[];
+}
+
+export default function List({ allFood }: ListProps) {
   return (
-    <section className="my-3 border rounded-2xl border-blue-400 w-full max-h-[300px] h-[200px] items-center gap-3 flex flex-col p-3">
-      <h2 className="text-4xl font-bold">맛집 리스트</h2>
-      <ul className="flex gap-3">
-        <li className="bg-amber-300 w-30 h-30">div1</li>
-        <li className="bg-amber-300 w-30 h-30">div1</li>
-        <li className="bg-amber-300 w-30 h-30">div1</li>
-        <li className="bg-amber-300 w-30 h-30">div1</li>
-      </ul>{' '}
+    <section className="my-3 border rounded-2xl border-blue-400 items-center gap-3 flex flex-col p-3 justify-center">
+      <h2 className="text-4xl font-bold ">맛집 리스트</h2>
+      <ul className="flex gap-3 flex-wrap justify-center">
+        {allFood.map((elm) => {
+          return (
+            <li key={elm.id} className="w-[150px] h-[150px] overflow-hidden my-2">
+              {elm.title}
+              <img
+                src={`http://localhost:3000/${elm.image.src}`}
+                alt={elm.image.alt}
+                className="object-cover"
+              />
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
