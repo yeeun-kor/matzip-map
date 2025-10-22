e40d78f302d5d67018ca16f157955477b7756188
yeeun <jovm268@naver.com>
2025-10-22 23:36:16 +0900

    feat : 사용자 위치 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,26 @@ import Loading from './components/Loading';
 const List = React.lazy(() => import('./components/List'));
 
 function App() {
+  //navigator 네비게이터 내장 객체 사용
+  //브라우저에서 navigator.geolocation.getCurrentPosition 메서드를 사용해 현재 위치를 가져오는 함수 생성
+  //coords값만 따로 분리
+
+  //- userLocation 호출하면, 전달받은 position값을 객체타입으로 따로 저장해주는 로직
+  const userLocation = (position) => {
+    const positionObj = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    console.log(positionObj);
+  };
+
+  //- 사용자의 위치를 가져오고 userLocation()함수 호출하는 로직
+  //getCurrentPosition (성공콜백함수, 에러콜백함수)
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(userLocation);
+  };
+
+  getUserLocation();
   return (
     <div className=" flex flex-col justify-center items-center mx-auto  gap-5">
       <main className="py-3 px-5 ">
